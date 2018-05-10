83906e3a5c728c61412abb20f2b6ac52ae3bac24
OleksandrSypalo <alexander.oprya406@gmail.com>
2018-05-11 02:37:08 +0300

    replace  env-DEV

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const nodeStatic = require('node-static');
 const path = require('path');
 const http = require('http');
 const PORT = 8082;
-const env = process.env.NODE_ENV || "development";
+const env = (process.argv[2] || false);
 
 const file = new nodeStatic.Server(path.join(__dirname, 'dist'), {
     cache: 60 * 60,
